e5748a5c52fb857e45b9055b4136bac0b118419c
李承斌 <15031617367@163.com>
2021-10-12 14:31:48 +0800

    dev

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,14 +1,20 @@
 /*eslint-disable */
 import React from "react";
-
+import "./index.scss"
 class Map extends React.Component {
   constructor(props) {
     super(props)
   }
+  componentDidMount(){
+    var map = new BMapGL.Map("container");
+    var point = new BMapGL.Point(116.404, 39.915);
+    map.centerAndZoom(point, 15); 
+  }
+
   render() {
     return (
-      <div>
-        地图组件
+      <div className="map">
+        <div id="container"></div>
       </div>
     )
   }
