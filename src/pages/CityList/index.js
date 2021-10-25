4a936d13362c95be5a8678bb87211045d93c1a8b
gongjian <225763904@qq.com>
2021-10-25 21:29:47 +0800

    完成城市列表的渲染和接口联调

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -5,7 +5,6 @@ import axios from "axios"
 import "./index.scss"
 import {getCurrentCity} from "../../utils/index"
 import { List, AutoSizer } from "react-virtualized"
-
 // 城市列表格式化
 const formatCityList = (data)=>{
   let CityList = {};
@@ -47,13 +46,18 @@ const formatCityIndex = (letter)=>{
 const title_height = 36;
 // 每个城市的高度
 const name_height = 50
+// 用于节流的闭包参数
+let valid = true
 class CityList extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       CityList : {},
-      CityListFirst : []
+      CityListFirst : [],
+      activeIndex:0
     }
+    // 创建ref对象
+    this.citylestComponent = React.createRef()
   } 
   // 获取城市列表数据
   async getCityList(){
@@ -66,11 +70,12 @@ class CityList extends React.Component {
       methods:'get',
       url:"http://localhost:7501/area/hot"
     })
-    CityList.hot = {...hotList.data.body};
+    CityList['hot'] = [...hotList.data.body];
+    CityListFirst.unshift('hot');
     // 获取当前定位
     const curCity = await getCurrentCity();
     CityList['#'] = [curCity];
-    CityListFirst.unshift('#');
+    CityListFirst.unshift("#");
     this.setState(()=>{
       return {
         CityList:CityList,
@@ -78,8 +83,9 @@ class CityList extends React.Component {
       }
     })
   }
-  componentDidMount(){
-    this.getCityList();
+  async componentDidMount(){
+    await this.getCityList();
+    this.citylestComponent.current.measureAllRows()
   }
   rowRenderer=({
     key, 
@@ -107,6 +113,42 @@ class CityList extends React.Component {
     const {CityList,CityListFirst} = this.state;
     return title_height + CityList[CityListFirst[index]].length * name_height
   }
+  // 渲染锚点列表
+  renderCityIndex(){
+    const {CityListFirst,activeIndex} = this.state;
+    return CityListFirst.map((item,index)=>(
+        <li className="city-index-item" key={index} onClick={()=>{
+          // this.setState({
+          //   activeIndex:index
+          // })
+          this.citylestComponent.current.scrollToRow(index)
+        }}> 
+          <span className={activeIndex==index?"index-active":""}>{item==='hot'?'热':item.toUpperCase()}</span>
+        </li>
+      )
+    )
+  }
+  // 节流用于点击锚点高亮错位的问题
+  throttle(startIndex){
+    return (()=>{
+      if(!valid){
+        return false 
+      }
+      valid = false
+      setTimeout(() => {
+        if(!(this.state.activeIndex == startIndex)){
+          this.setState({
+            activeIndex:startIndex
+          })
+        }
+        valid = true;
+      }, 0)
+    })()
+  }
+  // 用于获取List组件中渲染行的高亮
+  onRowsRendered = ({startIndex})=>{
+    this.throttle(startIndex)
+  }
   render() {
     return (
       <div className="cityList">
@@ -119,15 +161,24 @@ class CityList extends React.Component {
           {
             ({width,height}) => (
               <List
+                ref={this.citylestComponent}
                 width={width}
                 height={height-45}
                 rowCount={this.state.CityListFirst.length}
                 rowHeight={this.getRowHeight}
                 rowRenderer={this.rowRenderer}
+                onRowsRendered={this.onRowsRendered}
+                scrollToAlignment='start'
               />
            )
           }
         </AutoSizer>
+        {/* 锚点 */}
+        <ul className="city-index">
+          {
+            this.renderCityIndex()
+          }
+        </ul>
       </div>
     )
   }
