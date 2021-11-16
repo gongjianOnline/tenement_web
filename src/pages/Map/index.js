c8855700d316bf7b91fa572a7f1d1d648e8b2a75
李承斌 <1307437149@qq.com>
2021-11-16 10:00:53 +0800

    完成地图覆盖物绘制和事件添加

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import "./index.scss"
 import NavHeader from "../NavHeader/index"
+import styles from "./index.module.css"
 class Map extends React.Component {
   constructor(props) {
     super(props)
@@ -21,6 +22,35 @@ class Map extends React.Component {
         map.addControl(new BMapGL.ScaleControl());
         map.addControl(new BMapGL.ZoomControl());
         map.addControl(new BMapGL.CityListControl());
+        // 创建地图覆盖物
+        const opts = {
+          position:point,
+          offset:new BMapGL.Size(-35,-35)
+        }
+        /*设置setContent,第一个参数会失效,因此传空占位参数*/
+        const label = new BMapGL.Label("",opts);
+        label.setContent(`
+          <div class="${styles.bubble}">
+            <p class="${styles.name}">${name || "北京"}</p>
+            <p>0套</p>
+          </div>
+        `)
+        label.setStyle({
+          cursor:"pointer",
+          border:"0px solid rgb(255,0,0)",
+          padding:"0px",
+          whitsSpace:"nowrap",
+          color:"rgb(255,255,255)",
+          fontSize:"11px",
+          textAlign:"center"
+        })
+        // 添加点击事件
+        label.addEventListener("click",(e)=>{
+          console.log("打印点击事件",e)
+        })
+        map.addOverlay(label)
+        
+
       }
     },label)
   }
