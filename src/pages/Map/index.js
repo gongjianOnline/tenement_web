a6f8735c550a7560eb889ad5db9fcb637b563fdc
李承斌 <1307437149@qq.com>
2021-11-16 18:14:23 +0800

    createCircle封装完成

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -1,142 +1,170 @@
 /*eslint-disable */
 import React from "react";
-import "./index.scss"
-import NavHeader from "../NavHeader/index"
-import styles from "./index.module.css"
-import axios from "axios"
+import "./index.scss";
+import NavHeader from "../NavHeader/index";
+import styles from "./index.module.css";
+import axios from "axios";
 
 class Map extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
-  componentDidMount(){
+  componentDidMount() {
     // 获取当前定位城市
-    this.initMap()
+    this.initMap();
   }
 
-  initMap(){
-    const {label,value}  = JSON.parse(localStorage.getItem("hkzf_city"));
+  initMap() {
+    const { label, value } = JSON.parse(localStorage.getItem("hkzf_city"));
     const map = new BMapGL.Map("container");
-    this.map = map
+    this.map = map;
     const myGeo = new BMapGL.Geocoder();
-    myGeo.getPoint(label,async (point)=>{
-      if(point){
-        map.centerAndZoom(point,11);
-        map.enableScrollWheelZoom(true)
-        map.addControl(new BMapGL.ScaleControl());
-        map.addControl(new BMapGL.ZoomControl());
-        map.addControl(new BMapGL.CityListControl());
-        
-        this.renderOverlays(value)
+    myGeo.getPoint(
+      label,
+      async (point) => {
+        if (point) {
+          map.centerAndZoom(point, 11);
+          map.enableScrollWheelZoom(true);
+          map.addControl(new BMapGL.ScaleControl());
+          map.addControl(new BMapGL.ZoomControl());
+          map.addControl(new BMapGL.CityListControl());
 
+          this.renderOverlays(value);
 
-        // 获取房屋信息列表
-        // let response = await axios({
-        //   methods:"get",
-        //   url:"http://localhost:7501/area/map",
-        //   params:{
-        //     id:value
-        //   }
-        // })
-        // console.log("房屋列表",response)
-        // response.data.body.forEach((item,index)=>{
-        //   let arePoint = new BMapGL.Point(item.coord.longitude,item.coord.latitude)
-        //   /*设置setContent,第一个参数会失效,因此传空占位参数*/
-        //   const label = new BMapGL.Label("",{
-        //     position: arePoint,
-        //     offset:new BMapGL.Size(-35,-35)
-        //   });
-        //   label.setContent(`
-        //     <div class="${styles.bubble}">
-        //       <p class="${styles.name}">${item.label || "暂无"}</p>
-        //       <p>${item.count || "0"}套</p>
-        //     </div>
-        //   `)
-        //   label.setStyle({
-        //     cursor:"pointer",
-        //     border:"0px solid rgb(255,0,0)",
-        //     padding:"0px",
-        //     whitsSpace:"nowrap",
-        //     color:"rgb(255,255,255)",
-        //     fontSize:"11px",
-        //     textAlign:"center"
-        //   })
-        //   label.id = item.value
-        //   // 添加点击事件
-        //   label.addEventListener("click",(e)=>{
-        //     console.log("打印点击事件",e,label.id)
-        //     // 放大地图,当前点击的覆盖物为中心
-        //     map.centerAndZoom(arePoint,13)
-        //     map.clearOverlays()
-        //   })
-        //   map.addOverlay(label)
-        // })
-      }
-    },label)
+          // 获取房屋信息列表
+          // let response = await axios({
+          //   methods:"get",
+          //   url:"http://localhost:7501/area/map",
+          //   params:{
+          //     id:value
+          //   }
+          // })
+          // console.log("房屋列表",response)
+          // response.data.body.forEach((item,index)=>{
+          //   let arePoint = new BMapGL.Point(item.coord.longitude,item.coord.latitude)
+          //   /*设置setContent,第一个参数会失效,因此传空占位参数*/
+          //   const label = new BMapGL.Label("",{
+          //     position: arePoint,
+          //     offset:new BMapGL.Size(-35,-35)
+          //   });
+          //   label.setContent(`
+          //     <div class="${styles.bubble}">
+          //       <p class="${styles.name}">${item.label || "暂无"}</p>
+          //       <p>${item.count || "0"}套</p>
+          //     </div>
+          //   `)
+          //   label.setStyle({
+          //     cursor:"pointer",
+          //     border:"0px solid rgb(255,0,0)",
+          //     padding:"0px",
+          //     whitsSpace:"nowrap",
+          //     color:"rgb(255,255,255)",
+          //     fontSize:"11px",
+          //     textAlign:"center"
+          //   })
+          //   label.id = item.value
+          //   // 添加点击事件
+          //   label.addEventListener("click",(e)=>{
+          //     console.log("打印点击事件",e,label.id)
+          //     // 放大地图,当前点击的覆盖物为中心
+          //     map.centerAndZoom(arePoint,13)
+          //     map.clearOverlays()
+          //   })
+          //   map.addOverlay(label)
+          // })
+        }
+      },
+      label
+    );
   }
   // 渲染覆盖物入口
-  async renderOverlays(id){
+  async renderOverlays(id) {
     const response = await axios({
-      methods:'get',
-      url:'http://localhost:7501/area/map',
-      params:{
-        id:id
-      }
-    })
-    console.log("房屋列表",response)
+      methods: "get",
+      url: "http://localhost:7501/area/map",
+      params: {
+        id: id,
+      },
+    });
+    console.log("房屋列表", response);
     let res = response.data.body;
-    const {nextZoom,type} = this.getTypeAndZoom()
-    res.forEach((item)=>{
-      this.createOverlays(item,nextZoom,type)
-    })
+    const { nextZoom, type } = this.getTypeAndZoom();
+    res.forEach((item) => {
+      this.createOverlays(item, nextZoom, type);
+    });
   }
   // 获取当前地图的缩放级别
-  getTypeAndZoom(){
-    const zoom = this.map.getZoom()
-    let nextZoom,type
-    if(zoom >= 10 && zoom < 12){
+  getTypeAndZoom() {
+    const zoom = this.map.getZoom();
+    let nextZoom, type;
+    if (zoom >= 10 && zoom < 12) {
       //区
       // 下一个缩放级别
-      nextZoom = 13
-      type='circle'
-    }else if(zoom>=12 && zoom<14){
+      nextZoom = 13;
+      type = "circle";
+    } else if (zoom >= 12 && zoom < 14) {
       // 镇
-      nextZoom = 15
-      type= "circle"
-    }else if(zoom >= 14 && zoom < 16){
+      nextZoom = 15;
+      type = "circle";
+    } else if (zoom >= 14 && zoom < 16) {
       // 小区
-      type = "rect"
+      type = "rect";
     }
-    return {nextZoom,type}
+    return { nextZoom, type };
   }
   // 创建覆盖物
-  createOverlays(data,zoom,type){
+  createOverlays(data, zoom, type) {
     const {
-      coord:{longitude,latitude},
-      label:areaName,
+      coord: { longitude, latitude },
+      label: areaName,
       count,
-      value
-    } = data
+      value,
+    } = data;
     // 创建坐标对象
-    let areaPoint = new BMapGL.Point(longitude,latitude)
-
-    if(type === "circle"){
+    let areaPoint = new BMapGL.Point(longitude, latitude);
+    if (type === "circle") {
       // 创建小区和镇的覆盖物
-      this.createCircle(areaPoint,areaName,count,value,zoom)
-    }else{
+      this.createCircle(areaPoint, areaName, count, value, zoom);
+    } else {
       // 创建小区覆盖物
-      this.createRect(areaPoint,areaName,count,value)
+      this.createRect(areaPoint, areaName, count, value);
     }
   }
   // 创建区\镇覆盖物
-  createCircle(){
-
+  createCircle(point, name, count, id, zoom) {
+    const label = new BMapGL.Label("", {
+      position: point,
+      offset: new BMapGL.Size(-35, -35),
+    });
+    label.setContent(`
+      <div class="${styles.bubble}">
+        <p class="${styles.name}">${name || "暂无"}</p>
+        <p>${count || "0"}套</p>
+      </div>
+    `);
+    label.setStyle({
+      cursor: "pointer",
+      border: "0px solid rgb(255,0,0)",
+      padding: "0px",
+      whitsSpace: "nowrap",
+      color: "rgb(255,255,255)",
+      fontSize: "11px",
+      textAlign: "center",
+    });
+    label.id = id;
+    // 添加点击事件
+    label.addEventListener("click", (e) => {
+      // 调用 renderOverlays 方法, 获取该区域下的房源数据
+      this.renderOverlays(id)
+      // 放大地图,当前点击的覆盖物为中心
+      this.map.centerAndZoom(point, zoom);
+      this.map.clearOverlays();
+    });
+    this.map.addOverlay(label);
   }
+
   // 创建小区覆盖物
-  createRect(){
-
-  }
-
+  createRect() {}
 
   render() {
     return (
@@ -144,7 +172,7 @@ class Map extends React.Component {
         <NavHeader>地图找房</NavHeader>
         <div id="container"></div>
       </div>
-    )
+    );
   }
 }
-export default Map
+export default Map;
